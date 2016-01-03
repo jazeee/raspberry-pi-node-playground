be9a29db61bcc8151f1f2885015a896e801fe44c
Jaz Singh <jazeee@gmail.com>
2016-01-03 23:37:17 +0000

    Added variable time

--- test.sh
+++ test.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+TIME=$1
+
+echo "Running for $TIME"
+
 for i in 4 18 23 24; do gpio-admin export $i; done
 
 for i in 4 18 23 24; do echo out > /sys/class/gpio/gpio$i/direction ; done
 
 i=1; echo $i > /sys/class/gpio/gpio24/value && echo $i > /sys/class/gpio/gpio23/value
 
-sleep 2s
+sleep $TIME
 
 i=0; echo $i > /sys/class/gpio/gpio24/value && echo $i > /sys/class/gpio/gpio23/value
 
