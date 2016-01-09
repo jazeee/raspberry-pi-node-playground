f2972152fcf2f0feaf96192e60095ce680d69d06
Jaz Singh <jazeee@gmail.com>
2016-01-09 19:39:10 +0000

    Updated to dual h-bridge for bidirectional control

--- test.sh
+++ test.sh
@@ -4,14 +4,20 @@ TIME=$1
 
 echo "Running for $TIME"
 
-for i in 4 18 23 24; do gpio-admin export $i; done
+for i in 2 3 23 24; do gpio-admin export $i; done
 
-for i in 4 18 23 24; do echo out > /sys/class/gpio/gpio$i/direction ; done
+for i in 2 3 23 24; do echo out > /sys/class/gpio/gpio$i/direction ; done
 
-i=1; echo $i > /sys/class/gpio/gpio24/value && echo $i > /sys/class/gpio/gpio23/value
+i=1; echo $i > /sys/class/gpio/gpio24/value && echo $i > /sys/class/gpio/gpio2/value
 
 sleep $TIME
 
-i=0; echo $i > /sys/class/gpio/gpio24/value && echo $i > /sys/class/gpio/gpio23/value
+i=0; echo $i > /sys/class/gpio/gpio24/value && echo $i > /sys/class/gpio/gpio2/value
 
-for i in 4 18 23 24; do gpio-admin unexport $i; done
+i=1; echo $i > /sys/class/gpio/gpio23/value && echo $i > /sys/class/gpio/gpio3/value
+
+sleep $TIME
+
+i=0; echo $i > /sys/class/gpio/gpio23/value && echo $i > /sys/class/gpio/gpio3/value
+
+for i in 2 3 23 24; do gpio-admin unexport $i; done
