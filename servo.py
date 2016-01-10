37d33529f977817315ed5a00093414080036f106
Jaz Singh <jazeee@gmail.com>
2016-01-10 21:10:19 +0000

    Added python

---
+++ servo.py
@@ -0,0 +1,24 @@
+#!/bin/python
+
+from RPIO import PWM
+import sys
+import time
+
+servo = PWM.Servo()
+
+port = int(sys.argv[1])
+delay = int(sys.argv[2])
+
+print port 
+print delay
+
+
+position = 700
+while position < delay:
+	servo.set_servo(port, position)
+	position = position + 50
+	time.sleep(0.05)
+
+time.sleep(0.5)
+servo.set_servo(port, 700)
+time.sleep(1)
